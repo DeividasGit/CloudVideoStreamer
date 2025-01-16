6efdaf633fb8df7df36779a3ea9a5c0d7a70d2e8
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-01-16 16:06:37 +0200

    dbContext

---
+++ CloudVideoStreamer.Repository/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudVideoStreamer.Repository;
+
+public class AppDbContext : DbContext
+{
+  public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+  {
+    return base.SaveChangesAsync(cancellationToken);
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+  }
+}
