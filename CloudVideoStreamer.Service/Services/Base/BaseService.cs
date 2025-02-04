ee2cd96b907057b9f559f544391e6d0b30d9aff1
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-02-04 20:04:29 +0200

    Base Business logic layer

---
+++ CloudVideoStreamer.Service/Services/Base/BaseService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudVideoStreamer.Repository.Interfaces;
+using CloudVideoStreamer.Repository.Models.Base;
+using CloudVideoStreamer.Service.Interfaces.Base;
+using CloudVideoStreamer.Repository.Repositories;
+
+namespace CloudVideoStreamer.Service.Services.Base;
+
+public class BaseService<T, TK> : IBaseService<T, TK> where T : class, IBaseEntity<TK>
+{
+  private readonly IUnitOfWork _unitOfWork;
+
+  public BaseService(IUnitOfWork unitOfWork)
+  {
+    _unitOfWork = unitOfWork;
+  }
+
+  public IQueryable<T> GetAll()
+  {
+    return _unitOfWork.Repository<T, TK>().GetAll();
+  }
+}
