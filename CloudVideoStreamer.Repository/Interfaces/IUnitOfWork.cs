b7e1cb44f817c0a67c6274e2dffde99754bc24e6
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-02-04 20:03:34 +0200

    BaseRepository and UnitOfWork

---
+++ CloudVideoStreamer.Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using CloudVideoStreamer.Repository.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVideoStreamer.Repository.Interfaces;
+
+public interface IUnitOfWork
+{
+  IBaseRepository<T, TK> Repository<T, TK>() where T : class, IBaseEntity<TK>;
+  Task<int> SaveChangesAsync();
+}
---
+++ CloudVideoStreamer.Repository/Repositories/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudVideoStreamer.Repository.Interfaces;
+using CloudVideoStreamer.Repository.Models.Base;
+
+namespace CloudVideoStreamer.Repository.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+  private readonly AppDbContext _context;
+
+  public UnitOfWork(AppDbContext context)
+  {
+    _context = context;
+  }
+
+  public IBaseRepository<T, TK> Repository<T, TK>() where T : class, IBaseEntity<TK>
+  {
+    return new BaseRepository<T, TK>(_context);
+  }
+
+  public async Task<int> SaveChangesAsync()
+  {
+    return await _context.SaveChangesAsync();
+  }
+}
