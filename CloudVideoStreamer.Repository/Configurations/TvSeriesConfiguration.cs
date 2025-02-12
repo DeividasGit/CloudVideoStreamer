2e1d8c2e4fa62d228d12898996d62156ec4b29d3
DeividasGit <zem.deividas@gmail.com>
2025-02-12 16:16:51 +0200

    Configurations

---
+++ CloudVideoStreamer.Repository/Configurations/TvSeriesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CloudVideoStreamer.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CloudVideoStreamer.Repository.Configurations;
+
+public class TvSeriesConfiguration : IEntityTypeConfiguration<TvSeries>
+{
+  public void Configure(EntityTypeBuilder<TvSeries> builder)
+  {
+    builder.HasKey(x => x.Id);
+
+    builder.Property(x => x.SeasonCount);
+
+    builder
+      .HasOne(x => x.MediaContent)
+      .WithMany(x => x.TvSeries)
+      .HasForeignKey(x => x.MediaContentId)
+      .IsRequired()
+      .OnDelete(DeleteBehavior.Restrict);
+  }
+}
