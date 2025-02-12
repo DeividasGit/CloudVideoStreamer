db6fbd35ef0d9bb84c20be5d8cba35cfc0ad4d3a
DeividasGit <zem.deividas@gmail.com>
2025-02-12 16:16:34 +0200

    models

---
+++ CloudVideoStreamer.Repository/Models/MediaContent.cs
@@ -0,0 +1,14 @@
+﻿using CloudVideoStreamer.Repository.Models.Base;
+
+namespace CloudVideoStreamer.Repository.Models;
+
+public class MediaContent : IBaseEntity<int>
+{
+  public int Id { get; set; }
+  public string Title { get; set; }
+  public string Description { get; set; }
+  public DateTime ReleaseDate { get; set; }
+  public decimal Rating { get; set; }
+  public ICollection<Movie> Movies { get; set; }
+  public ICollection<TvSeries> TvSeries { get; set; }
+}
