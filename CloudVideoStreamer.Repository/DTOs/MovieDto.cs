5d4912716dc0182fe71a57fe3abf174bfe00c6ca
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-02-13 12:20:52 +0200

    DTOs

---
+++ CloudVideoStreamer.Repository/DTOs/MovieDto.cs
@@ -0,0 +1,9 @@
+﻿using CloudVideoStreamer.Repository.Models;
+
+namespace CloudVideoStreamer.Repository.DTOs;
+
+public class MovieDto
+{
+  public decimal DurationInSeconds { get; set; }
+  public required MediaContentDto MediaContent { get; set; }
+}
