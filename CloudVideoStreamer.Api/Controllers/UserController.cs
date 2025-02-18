d39b6bdf7c530638036b8e9a329d51d6342356af
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-02-18 15:30:09 +0200

    user controller

---
+++ CloudVideoStreamer.Api/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using CloudVideoStreamer.Api.Controllers.Base;
+using CloudVideoStreamer.Repository.Models;
+using CloudVideoStreamer.Service.Interfaces;
+using CloudVideoStreamer.Service.Interfaces.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudVideoStreamer.Api.Controllers {
+  public class UserController : BaseController<User, int> {
+    
+    IUserService _userService;
+
+    public UserController(IUserService userService) : base(userService) 
+    {
+      _userService = userService;
+    }
+  }
+}
---
+++ CloudVideoStreamer.Service/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using CloudVideoStreamer.Repository.Interfaces;
+using CloudVideoStreamer.Repository.Models;
+using CloudVideoStreamer.Service.Interfaces;
+using CloudVideoStreamer.Service.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVideoStreamer.Service.Services {
+  public class UserService : BaseService<User, int>, IUserService {
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UserService(IUnitOfWork unitOfWork) : base(unitOfWork) 
+    {
+      _unitOfWork = unitOfWork;
+    }
+  }
+}
