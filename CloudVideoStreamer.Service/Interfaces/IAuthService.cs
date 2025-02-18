1c7e3366799ae6a67020e0c885f0f4b300020f5b
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-02-18 15:18:12 +0200

    token generation

---
+++ CloudVideoStreamer.Service/Interfaces/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using CloudVideoStreamer.Repository.DTOs;
+using CloudVideoStreamer.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVideoStreamer.Service.Interfaces {
+  public interface IAuthService 
+  {
+    string GenerateAccessToken(User user);
+    string GenerateRefreshToken();
+    Task<User> GetUser(UserLoginDto model);
+  }
+}
