523bd2f893c5c4810ddbde44b6ac771d6333f7fe
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-02-18 13:12:40 +0200

    MediaContent

---
+++ CloudVideoStreamer.Api/Controllers/MediaContentController.cs
@@ -0,0 +1,18 @@
+﻿using CloudVideoStreamer.Api.Controllers.Base;
+using CloudVideoStreamer.Repository.Models;
+using CloudVideoStreamer.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudVideoStreamer.Api.Controllers {
+  [ApiController]
+  [Route("api/[controller]")]
+  public class MediaContentController : BaseController<MediaContent, int> 
+  {
+    private readonly IMediaContentService _mediaContentService;
+
+    public MediaContentController(IMediaContentService mediaContentService) : base(mediaContentService)
+    {
+      _mediaContentService = mediaContentService;
+    }
+  }
+}
