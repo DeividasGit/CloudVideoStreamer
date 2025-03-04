4b483e618d76a8b11ac9f311a6e2ab13b9663543
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-03-04 17:04:09 +0200

    configs

---
+++ CloudVideoStreamer.Repository/Configurations/GenreConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CloudVideoStreamer.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVideoStreamer.Repository.Configurations
+{
+  public class GenreConfiguration : IEntityTypeConfiguration<Genre>
+  {
+    public void Configure(EntityTypeBuilder<Genre> builder)
+    {
+      builder.HasKey(x => x.Id);
+
+      builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+    }
+  }
+}
---
+++ CloudVideoStreamer.Repository/Configurations/MediaContentGenreConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using CloudVideoStreamer.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVideoStreamer.Repository.Configurations
+{
+  public class MediaContentGenreConfiguration : IEntityTypeConfiguration<MediaContentGenre>
+  {
+    public void Configure(EntityTypeBuilder<MediaContentGenre> builder)
+    {
+      builder.HasKey(x => x.Id);
+
+      builder
+        .HasOne(x => x.MediaContent)
+        .WithMany(x => x.Genres)
+        .HasForeignKey(x => x.MediaContentId)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Restrict);
+
+      builder
+        .HasOne(x => x.Genre)
+        .WithMany(x => x.MediaContents)
+        .HasForeignKey(x => x.GenreId)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Restrict);
+    }
+  }
+}
