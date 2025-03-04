f7a6cc608ccca1a028763daad1ae9da40854aa4e
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-03-04 15:05:57 +0200

    migration

---
+++ CloudVideoStreamer.Repository/Migrations/20250304122937_roles.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudVideoStreamer.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, defaultValue: "User")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RoleId",
+                table: "User",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Role_RoleId",
+                table: "User",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Role_RoleId",
+                table: "User");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_RoleId",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "User");
+        }
+    }
+}
