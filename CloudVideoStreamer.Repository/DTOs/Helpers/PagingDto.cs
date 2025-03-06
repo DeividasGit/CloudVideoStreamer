f5496de145ae9b6fbc54d80323438ed132ad245e
Deividas Žemeckas <deividas.zemeckas@citybee.lt>
2025-03-06 17:00:01 +0200

    paging and sorting

---
+++ CloudVideoStreamer.Repository/DTOs/Helpers/PagingDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVideoStreamer.Repository.DTOs.Paging
+{
+  public class PagingDto
+  {
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+  }
+}
---
+++ CloudVideoStreamer.Service/Helpers/PagingHelper.cs
@@ -0,0 +1,19 @@
+﻿using CloudVideoStreamer.Repository.DTOs.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVideoStreamer.Service.Helpers
+{
+  public static class PagingHelper
+  {
+    public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> query, PagingDto paging)
+    {
+      if (paging == null) return query;
+
+      return query.Skip((paging.PageNumber - 1) * paging.PageSize).Take(paging.PageSize);
+    }
+  }
+}
