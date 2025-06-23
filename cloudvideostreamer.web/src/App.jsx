9c257e8fa2feb97454be7e19abfa5447136ca542
Deividas <deividas.zemeckas@oixio.eu>
2025-06-23 12:15:13 +0300

    routing

--- cloudvideostreamer.web/src/App.jsx
+++ cloudvideostreamer.web/src/App.jsx
@@ -1,16 +1,11 @@
-import { useReducer, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider } from 'react-router-dom';
 import './App.css'
+import { router } from './routes';
 
 function App() {
-  //const [count, setCount] = useState(0);
-  const [count, incrementCount] = useReducer((count) => count + 1, 0);
 
   return (
-    <button onClick={incrementCount}>
-      { count }
-    </button>
+      <RouterProvider router = { router } />
   );
 }
 
